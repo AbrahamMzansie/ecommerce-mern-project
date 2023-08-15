7f38933518eae5dbd5bac0a2db5f2b0f0a548863
AbrahamMzansie <abraham.nkomo@gmail.com>
2023-08-15 22:41:53 +0200

    added css property

--- frontend/src/components/ProductCarousel.jsx
+++ frontend/src/components/ProductCarousel.jsx
@@ -22,7 +22,7 @@ const ProductCarousel = () => {
         <Carousel.Item key={product._id}>
           <Link to={`/product/${product._id}`}>
             <Image src={product.image} alt={product.name} fluid />
-            <Carousel.Caption className="carousel-caption">
+            <Carousel.Caption className="carousel-caption img-fluid-racousel">
               <h2 className="text-white text-right">
                 {product.name} (${product.price})
               </h2>
